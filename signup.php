9adfcf97fb54d1e65832bd2106492fb7a6594311
Bilal DAĞ <daagbilal@hotmail.com>
2023-11-28 23:29:18 +0300

    commit

--- signup.php
+++ signup.php
@@ -111,17 +111,6 @@ if (($_SERVER["REQUEST_METHOD"]) == "POST") {
 </head>
 
 <body>
-    <div id="loading">
-        <img src="icons/Double Ring-1s-200px.svg" alt="Yükleniyor..." />
-    </div>
-
-    <script>
-        window.addEventListener('load', fg_load)
-
-        function fg_load() {
-            document.getElementById('loading').style.display = 'none'
-        }
-    </script>
     <div class="login">
         <div class="log">
             <div class="header">
